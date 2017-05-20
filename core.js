a201958fb727adb47d810737d01d8e790840aeaf
Shawn <sgraven66@gmail.com>
2017-05-19 20:05:49 -0500

    Create core.js

---
+++ core.js
@@ -0,0 +1,69 @@
+
+function replace(){
+	
+	st= st.split('')
+	while(typeof st[i]=== 'string'){
+		i++
+		switch(){
+			case 'a':
+				st[i]= 'ka';
+			case 'b':
+				st[i]= 'zu';
+			case 'c':
+				st[i]= 'mi';
+			case 'd':
+				st[i]= 'te';
+			case 'e':
+				st[i]= 'ku';
+			case 'f':
+				st[i]= 'lu';
+			case 'g':
+				st[i]= 'ji';
+			case 'h':
+				st[i]= 'ri';
+			case 'i':
+				st[i]= 'ki';
+			case 'j':
+				st[i]= 'zu';
+			case 'k':
+				st[i]= 'me';
+			case 'l':
+				st[i]= 'ta';
+			case 'm':
+				st[i]= 'rin';
+			case 'n':
+				st[i]= 'to';
+			case 'o':
+				st[i]= 'mo';
+			case 'p':
+				st[i]= 'no';
+			case 'q':
+				st[i]= 'ke';
+			case 'r':
+				st[i]= 'shi';
+			case 's':
+				st[i]= 'ari';
+			case 't':
+				st[i]= 'chi';
+			case 'u':
+				st[i]= 'do';
+			case 'v':
+				st[i]= 'ru';
+			case 'w':
+				st[i]= 'mei';
+			case 'x':
+				st[i]= 'na';
+			case 'y':
+				st[i]= 'fu';
+			case 'z':
+				st[i]= 'zi';
+			case ' ':
+				st[i]= ' ';
+			defult:
+				console.log(st[i]);
+				st[i]= ' ';
+		}
+		
+	}
+	st= st.toString
+}
