5f105bfa7155eabe2860385daa019eccfa2a5bd6
Shawn <sgraven66@gmail.com>
2017-05-25 20:49:33 -0500

    improved "" and unicode support
    Now does not crash on """"""""""
    Also improved code

--- core.js
+++ core.js
@@ -1,150 +1,212 @@
 "strict mode";
-var st, i, upper, C;
+var st, i, upper, C, I;
 function act(id){
-	C= '';
+	C= [''];
 	st=document.getElementById('string').value;
 	st= st.split('');
-	i=0;
+	I= i=0;
 	while(typeof st[i]=== 'string'){
-		if (st[i]== st[i].toUpperCase()){upper= true; st[i]= st[i].toLowerCase();}
-		else{upper= false;}
-		// on "n" where n is a character, it is NOT converted!
-		if(st[i - 1]+ st[i + 1]== '""'){
-			st[i - 1]= st[i + 1]= '';
+		var skip= false;
+		// "" not converted
+		if(st[i - 1]== '"' && st[i]!= '"'){
+			if(st[i + 1]== '"'){
+				expt(1);
+				skip= true;
+			}
+			else if(st[i + 2]== '"'){
+				expt(2);
+				skip= true;
+			}
+			else if(st[i + 3]== '"'){
+				expt(3);
+				skip= true;
+			}
+			else if(st[i + 4]== '"'){
+				expt(4);
+				skip= true;
+			}
+			else if(st[i + 5]== '"'){
+				expt(5);
+				skip= true;
+			}
+			else if(st[i + 6]== '"'){
+				expt(6);
+				skip= true;
+			}
+			else if(st[i + 7]== '"'){
+				expt(7);
+				skip= true;
+			}
+			else if(st[i + 8]== '"'){
+				expt(8);
+				skip= true;
+			}
+			else if(st[i + 9]== '"'){
+				expt(9);
+				skip= true;
+			}
+			else if(st[i + 10]== '"'){
+				expt(10);
+				skip= true;
+			}
+			else if(st[i + 11]== '"'){
+				expt(11);
+				skip= true;
+			}
+			else if(st[i + 12]== '"'){
+				expt(12);
+				skip= true;
+			}
 		}
-		else if(st[i - 1]== '&' && st[i + 2]== ';' ){}
-		else if(st[i - 2]== '&' && st[i + 1]== ';' ){}
-		
-		else if(st[i - 1]== '&' && st[i + 3]== ';' ){}
-		else if(st[i - 2]== '&' && st[i + 2]== ';' ){}
-		else if(st[i - 3]== '&' && st[i + 1]== ';' ){}
-		
-		else if(st[i - 1]== '&' && st[i + 4]== ';' ){}
-		else if(st[i - 2]== '&' && st[i + 3]== ';' ){}
-		else if(st[i - 3]== '&' && st[i + 2]== ';' ){}
-		else if(st[i - 4]== '&' && st[i + 1]== ';' ){}
-		
-		/*   6-a=b
-		var a=1,
-		b;
-		while(a< 6){
-			b= 6-a;
-			else if(st[i - a]== '&' && st[i + b]== ';' ){}
-			a++;
-		}*/
-		else if(st[i - 1]== '&' && st[i + 5]== ';' ){}
-		else if(st[i - 2]== '&' && st[i + 4]== ';' ){}
-		else if(st[i - 3]== '&' && st[i + 3]== ';' ){}
-		else if(st[i - 4]== '&' && st[i + 2]== ';' ){}
-		else if(st[i - 5]== '&' && st[i + 1]== ';' ){}
-		
-		else if(st[i - 1]== '&' && st[i + 6]== ';' ){}
-		else if(st[i - 2]== '&' && st[i + 5]== ';' ){}
-		else if(st[i - 3]== '&' && st[i + 4]== ';' ){}
-		else if(st[i - 4]== '&' && st[i + 3]== ';' ){}
-		else if(st[i - 5]== '&' && st[i + 2]== ';' ){}
-		else if(st[i - 6]== '&' && st[i + 1]== ';' ){}
-		
-		else{
-		switch(st[i]){
-			case 'a':
-				st[i]= 'ka';
-		       		break;
-			case 'b':
-				st[i]= 'zu';
-		       		break;
-			case 'c':
-				st[i]= 'mi';
-		       		break;
-			case 'd':
-				st[i]= 'te';
-		       		break;
-			case 'e':
-				st[i]= 'ku';
-		       		break;
-			case 'f':
-				st[i]= 'lu';
-		       		break;
-			case 'g':
-				st[i]= 'ji';
-		       		break;
-			case 'h':
-				st[i]= 'ri';
-		       		break;
-			case 'i':
-				st[i]= 'ki';
-		       		break;
-			case 'j':
-				st[i]= 'zu';
-		       		break;
-			case 'k':
-				st[i]= 'me';
-		       		break;
-			case 'l':
-				st[i]= 'ta';
-		       		break;
-			case 'm':
-				st[i]= 'rin';
-		       		break;
-			case 'n':
-				st[i]= 'to';
-		       		break;
-			case 'o':
-				st[i]= 'mo';
-		       		break;
-			case 'p':
-				st[i]= 'no';
-		       		break;
-			case 'q':
-				st[i]= 'ke';
-		       		break;
-			case 'r':
-				st[i]= 'shi';
-		       		break;
-			case 's':
-				st[i]= 'ari';
-		       		break;
-			case 't':
-				st[i]= 'chi';
-		       		break;
-			case 'u':
-				st[i]= 'do';
-		       		break;
-			case 'v':
-				st[i]= 'ru';
-		       		break;
-			case 'w':
-				st[i]= 'mei';
-		       		break;
-			case 'x':
-				st[i]= 'na';
-		       		break;
-			case 'y':
-				st[i]= 'fu';
-		       		break;
-			case 'z':
-				st[i]= 'zi';
-		       		break;
-			case '>':
-				st[i]= '&gt;'
-				break;
-			case '<':
-				st[i]= '&lt;'
-			case ' ':
-			case "'":
-			case '"':
-			case '_':
-			case '-':
-			case '.':
-			case ',':
-			case '':
-				upper= false;
-				break;
-			default:
-				upper= false;
-				C+= st[i];
-				break;
+		//Unicode
+		if(st[i - 1]== '&'){
+			if(st[i + 1]== ';' ){
+				exptA(1);
+				skip= true;
+			}
+			else if(st[i + 2]== ';' ){
+				exptA(2);
+				skip= true;
+			}
+			else if(st[i + 3]== ';' ){
+				exptA(3);
+				skip= true;
+			}
+			else if(st[i + 4]== ';' ){
+				exptA(4);
+				skip= true;
+			}
+			else if(st[i + 5]== ';' ){
+				exptA(5);
+				skip= true;
+			}
+			else if(st[i + 6]== ';' ){
+				exptA(6);
+				skip= true;
+			}
+			else if(st[i + 7]== ';' ){
+				exptA(7);
+				skip= true;
+			}
 		}
+		//igrore numbers
+		//if(!isNaN(st[i])){skip= true;}
+		if(skip== false){
+			if (st[i]== st[i].toUpperCase()){
+				upper= true;
+				st[i]= st[i].toLowerCase();
+			}
+			else{upper= false;}
+			switch(st[i]){
+				case 'a':
+					st[i]= 'ka';
+						break;
+				case 'b':
+					st[i]= 'zu';
+						break;
+				case 'c':
+					st[i]= 'mi';
+						break;
+				case 'd':
+					st[i]= 'te';
+						break;
+				case 'e':
+					st[i]= 'ku';
+						break;
+				case 'f':
+					st[i]= 'lu';
+						break;
+				case 'g':
+					st[i]= 'ji';
+						break;
+				case 'h':
+					st[i]= 'ri';
+						break;
+				case 'i':
+					st[i]= 'ki';
+						break;
+				case 'j':
+					st[i]= 'zu';
+						break;
+				case 'k':
+					st[i]= 'me';
+						break;
+				case 'l':
+					st[i]= 'ta';
+						break;
+				case 'm':
+					st[i]= 'rin';
+						break;
+				case 'n':
+					st[i]= 'to';
+						break;
+				case 'o':
+					st[i]= 'mo';
+						break;
+				case 'p':
+					st[i]= 'no';
+						break;
+				case 'q':
+					st[i]= 'ke';
+						break;
+				case 'r':
+					st[i]= 'shi';
+						break;
+				case 's':
+					st[i]= 'ari';
+						break;
+				case 't':
+					st[i]= 'chi';
+						break;
+				case 'u':
+					st[i]= 'do';
+						break;
+				case 'v':
+					st[i]= 'ru';
+						break;
+				case 'w':
+					st[i]= 'mei';
+						break;
+				case 'x':
+					st[i]= 'na';
+						break;
+				case 'y':
+					st[i]= 'fu';
+						break;
+				case 'z':
+					st[i]= 'zi';
+						break;
+				case '>':
+					st[i]= '&gt;'
+					console.log(st[i]);
+					upper= false;
+					C[I]= st[i];
+					I++;
+					break;
+				case '<':
+					st[i]= '&lt;'
+					console.log(st[i]);
+					upper= false;
+					C[I]= st[i];
+					I++;
+				case ' ':
+				case "'":
+				case '"':
+				case '_':
+				case '-':
+				case '.':
+				case ',':
+				case '':
+				case '&':
+					upper= false;
+					break;
+				default:
+					console.log(st[i]);
+					upper= false;
+					C[I]= st[i];
+					I++;
+					break;
+			}
 		}
 		if(upper== true){
 			st[i]= st[i].split('');
@@ -159,8 +221,7 @@ function act(id){
 		st= '<small style="font:10vh Orbitron">Name not Defined</small>'
 	}
 	document.getElementById(id).innerHTML= st;
-	if(C!= ''){
-		C= C.split('');
+	if(C!= '' || st.includes('&')){
 		C= C.sort();
 		var c= ['error'],
 		z= y=0;
@@ -183,3 +244,50 @@ function act(id){
 	else{document.getElementById('odd').innerHTML= 'No Problem characters Detected';}
 	c= C= y= z= st= i= upper= null;
 }
+
+
+function expt(e){
+	var vl= i + 1;
+	while(vl< e + i){
+		st[i]+= st[vl];
+		st[vl]='';
+		vl++;
+	}
+	st[i - 1]= '';
+	st[i + e]= '';
+	i+= e;
+}
+function exptA(e){
+	var vl= i + 1;
+	while(vl<= e + i){
+		st[i]+= st[vl];
+		st[vl]='';
+		vl++;
+	}
+	C[I]= '&'+ st[i];
+	I++;
+	i+= e;
+	//skip= true;//Does not work!
+}
+
+
+
+		/*Test for "n"  Failed
+		var lp=1;
+		else if(
+			while(lp<= 20){
+				if(st[i - 1]+ st[i + lp]== '""'){
+					break;
+				}
+				lp++;
+			})
+			{
+				
+				var li=1;
+				var text= st[i];
+				while(li< lp){
+					text+= st[i + li];
+					st[i + li]= '';
+				}
+			st[lp]= '';
+			}*/
