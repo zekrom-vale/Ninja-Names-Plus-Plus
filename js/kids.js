dddac28a285dec3f8f8da7596b7d05ebd13af925
zekrom-vale <sgraven66@gmail.com>
2017-06-06 13:18:38 -0500

    Removed redundent switch

--- js/kids.js
+++ js/kids.js
@@ -25,44 +25,7 @@ function RanChar(){
 	if(document.getElementById('ability').innerHTML== 'undefined' || document.getElementById('ability').innerHTML== 'undefined')color();
 	
 	num= num%8;
-	switch(num){
-		case 0:
-			document.getElementById('ability2').innerHTML=ch[0];
-			ch.splice(0, 1);
-			break;
-		case 1:
-			document.getElementById('ability2').innerHTML=ch[1];
-			ch.splice(1, 1);
-			break;
-		case 2:
-			document.getElementById('ability2').innerHTML=ch[2];
-			ch.splice(2, 1);
-			break;
-		case 3:
-			document.getElementById('ability2').innerHTML=ch[3];
-			ch.splice(3, 1);
-			break;
-		case 4:
-			document.getElementById('ability2').innerHTML=ch[4];
-			ch.splice(4, 1);
-			break;
-		case 5:
-			document.getElementById('ability2').innerHTML=ch[5];
-			ch.splice(5, 1);
-			break;
-		case 6:
-			document.getElementById('ability2').innerHTML=ch[6];
-			ch.splice(6, 1);
-			break;
-		case 7:
-			document.getElementById('ability2').innerHTML=ch[7];
-			ch.splice(7, 1);
-			break;
-		default:
-			document.getElementById('ability2').innerHTML=ch[ran(7)];
-			console.log('Error: Invalid length');
-			ch.splice(Ran, 1);
-			break;
-	}
+	document.getElementById('ability2').innerHTML=ch[num];
+	ch.splice(num, 1);
 	console.log('My super duper secret ninja ability is '+ ch[ran(6)]+ '!');
 }
